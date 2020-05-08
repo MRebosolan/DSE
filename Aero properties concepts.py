19d812e1c7cb40ea573af1ebf6142e5926432a3b
jornv <jornvanbeek143@gmail.com>
2020-05-08 11:37:35 +0200

    started class

--- Aero properties concepts.py
+++ Aero properties concepts.py
@@ -1,15 +1,29 @@
 from math import cos, radians
 
+class wing:
 
-def CLdes(q, W_start_cruise, W_end_cruise, S, sweep): #finds design cruise CL for aircraft and airfoil
-    CL = 1.1/1 * (0.5 * (W_start_cruise/S + W_end_cruise/S))
-    Cl = CL/(cos(radians(sweep)))^2
-    return (CL, Cl)
+    def __init__(self, sweep, W_start_cruise, W_end_cruise, S, chord_t, chord_r, wingspan):
+        self.sweep = sweep
+        self.W_start_cruise = W_start_cruise
+        self.W_end_cruise = W_end_cruise
+        self.S = S
+        self.chord_t = chord_t
+        self.chord_r = chord_r
+        self.wingspan = wingspan
 
 
-def taper (chord_r, chord_t):
-    return chord_t/chord_r
+    def CLdes(self, q, W_start_cruise, W_end_cruise, S, sweep): #finds design cruise CL for aircraft and airfoil
+        CL = 1.1/1 * (0.5 * (W_start_cruise/S + W_end_cruise/S))
+        cl = CL/(cos(radians(sweep)))^2
+        self.CL = CL
+        self.cl = cl
 
-def MAC (taper, chord_r):
-    mac = (2/3)*chord_r*((1+taper+taper**2)/(1+taper))
-    return mac
+
+
+    def taper (self, chord_r, chord_t):
+        self.taper = chord_t/chord_r
+
+    def MAC (self, taper, chord_r):
+        MAC = (2/3)*chord_r*((1+taper+taper**2)/(1+taper))
+        self.MAC = MAC
+
