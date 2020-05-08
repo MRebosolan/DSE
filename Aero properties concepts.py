dbdfce59e0a13bf13f7ab2cd36a3607704c7b7b6
jornv <jornvanbeek143@gmail.com>
2020-05-08 11:31:15 +0200

    added taper added MAC

--- Aero properties concepts.py
+++ Aero properties concepts.py
@@ -5,3 +5,11 @@ def CLdes(q, W_start_cruise, W_end_cruise, S, sweep): #finds design cruise CL fo
     CL = 1.1/1 * (0.5 * (W_start_cruise/S + W_end_cruise/S))
     Cl = CL/(cos(radians(sweep)))^2
     return (CL, Cl)
+
+
+def taper (chord_r, chord_t):
+    return chord_t/chord_r
+
+def MAC (taper, chord_r):
+    mac = (2/3)*chord_r*((1+taper+taper**2)/(1+taper))
+    return mac
--- aerodynamic_sizing.py
+++ aerodynamic_sizing.py
@@ -1,6 +1,15 @@
 #file for calculating several wing parameters
 # authors: Matteo & Jorn
 
+wingspan = 23.94 #m
+wingspan_2 = 27.0 #m, value if we use foldable wingtips
 def cldesign(q, W_S_start, W_S_end):
     CL_design = 1.1*(1/q)*(0.5*(W_S_start+W_S_end))
     return CL_design
+
+def taper (chord_r, chord_t):
+    return chord_t/chord_r
+
+def MAC (taper, chord_r):
+    mac = (2/3)*chord_r*((1+taper+taper**2)/(1+taper))
+    return mac
