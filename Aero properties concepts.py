394875f43c3f10c2280f9de9d34ef926c1924360
Matteo Rebosolan <m.rebosolan@student.tudelft.nl>
2020-05-08 13:51:54 +0200

    ladida

--- Aero properties concepts.py
+++ Aero properties concepts.py
@@ -1,5 +1,14 @@
 from math import cos, radians
 
+def atmosphere_calculator(h):
+    T_grad = -0.0065
+    T = 288.15 + T_grad*h
+    P = 101300 * (T/288.15)**(-9.81/(T_grad*287))
+    rho = P/(T*287)
+    a = (1.4*287*T)**0.5
+    return (T,P,rho,a)
+
+
 class wing:
 
     def __init__(self, sweep, W_start_cruise, W_end_cruise, S, chord_t, chord_r, wingspan):
@@ -13,7 +22,7 @@ class wing:
 
 
     def CLdes(self, q, W_start_cruise, W_end_cruise, S, sweep): #finds design cruise CL for aircraft and airfoil
-        CL = 1.1/1 * (0.5 * (W_start_cruise/S + W_end_cruise/S))
+        CL = 1.1/q * (0.5 * (W_start_cruise/S + W_end_cruise/S))
         cl = CL/(cos(radians(sweep)))^2
         self.CL = CL
         self.cl = cl
@@ -27,3 +36,6 @@ class wing:
         MAC = (2/3)*chord_r*((1+taper+taper**2)/(1+taper))
         self.MAC = MAC
 
+    def root_chord_tip_chord(self, S, wingspan, taper):
+        self.chord_r = (2*S)/(wingspan*(1+taper))
+        self.chord_t = self.chord_r * taper
